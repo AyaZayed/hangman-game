9891f2e372c8cf549f154414b56fba8f848b1aa0
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-06-29 23:12:29 +0300

    styling tweaks

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,7 +5,7 @@ export default function Popup({ word, wrongLetters, playAgain }) {
         <div class="popup-container" id="popup-container">
             <div class="popup">
                 <h2 id="final-message">
-                    {wrongLetters.length >= 6 ? 'Unfortunately you lost.' : 'Congratulations! You won.'}
+                    {wrongLetters.length >= 6 ? 'Unfortunately you lost.  😕' : 'Congratulations! You won!  😃'}
                 </h2>
                 <h3 id="final-message-reveal-word">...the word was: {word}</h3>
                 <button id="play-button" onClick={() => playAgain()}>Play Again</button>
